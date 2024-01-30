e0d2b832e926944680d6e02e54220dbd56407b6a
anushka bhandari <bhandarianushka670@gmail.com>
2024-01-30 22:52:17 +0530

    API for get all tasks

--- backend/Routes/TaskRoute.js
+++ backend/Routes/TaskRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { isAuthenticUser } = require("../Middleware/Auth");
 const {} = require("../Controllers/UserController");
-const { createTask } = require("../Controllers/TaskController");
+const { createTask,getAllTasks } = require("../Controllers/TaskController");
 const router = express.Router();
 
 router.post("/create", isAuthenticUser, createTask);
+router.get("/all", isAuthenticUser, getAllTasks);
 
 module.exports = router;

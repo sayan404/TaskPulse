e871d19e192f96056a6622964e872b767c185ede
JavidSumra <javidsumra135@gmail.com>
2024-01-31 14:38:12 +0530

    Solving Merge Conflict

--- backend/Routes/TaskRoute.js
+++ backend/Routes/TaskRoute.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const { isAuthenticUser } = require("../Middleware/Auth");
 const {} = require("../Controllers/UserController");
-const { createTask, deleteTask } = require("../Controllers/TaskController");
+const {
+  createTask,
+  deleteTask,
+  getAllTasks,
+} = require("../Controllers/TaskController");
 const router = express.Router();
 
 router.post("/create", isAuthenticUser, createTask);
+router.get("/all", isAuthenticUser, getAllTasks);
 router.delete("/delete/:id", isAuthenticUser, deleteTask);
 
 module.exports = router;

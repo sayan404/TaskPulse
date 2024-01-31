848b41767221c3fbeff0ff726ca92da64762310d
JavidSumra <javidsumra135@gmail.com>
2024-01-31 10:28:34 +0530

    Updatig Routes

--- backend/Routes/TaskRoute.js
+++ backend/Routes/TaskRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { isAuthenticUser } = require("../Middleware/Auth");
 const {} = require("../Controllers/UserController");
-const { createTask } = require("../Controllers/TaskController");
+const { createTask, deleteTask } = require("../Controllers/TaskController");
 const router = express.Router();
 
 router.post("/create", isAuthenticUser, createTask);
+router.delete("/delete/:id", isAuthenticUser, deleteTask);
 
 module.exports = router;

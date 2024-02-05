896e016cc38d3c9b077ec50f4395cbc56397db36
JavidSumra <javidsumra135@gmail.com>
2024-02-05 21:51:36 +0530

    Adding API Endpoint for Account Delete

--- backend/Routes/UserRoute.js
+++ backend/Routes/UserRoute.js
@@ -5,6 +5,7 @@ const {
   logout,
   getUserDetails,
   registerUser,
+  deleteAccount,
 } = require("../Controllers/UserController");
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticUser, getUserDetails);
+router.route("/account/delete/:id").delete(isAuthenticUser, deleteAccount);
 
 module.exports = router;
